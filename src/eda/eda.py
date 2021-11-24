5d0e4c953fee147dcebdd2a5501dc8cad399dae6
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2021-11-24 12:21:02 -0800

    Add get summary table function

--- src/eda/eda.py
+++ src/eda/eda.py
@@ -1,26 +1,46 @@
 # author: DSCI_522_group_28
 # date: 
 
-"""This script constructs various exploratory data visualizations,
+'''This script constructs various exploratory data visualizations,
 and tables.
 
-Usage: eda.py --train_df=<train_df> --output_file=<output_file>
+Usage: eda.py --train_path=<train_path> --outputfile=<outputfile>
 
 Options:
---train_df=<train_df>
+--train_pathf=<train_path>
 --output=<output>
-""""
+'''
 
 
-from doctop import docopt 
-import altair as alt
+from docopt import docopt 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
 
 opt = docopt(__doc__)
 
-def main(train, output):
+def main(train_path, outputfile):
   
+  
+  df = pd.read_csv("../../data/raw/abalone.data")
+  
+  train_df, test_df = train_test_split(df, test_size=0.2, random_state=123)
+  # If a directory path doesn't exist, create one
+ # if not os.path.exists(os.path.dirname(outputfile)):
+  #      os.makedirs(os.path.dirname(outputfile)) 
+        
+  # Develop human-readable column names
+  
+  
+  # Develop and save summary statistic table
+  
+  # Creates figure showing distribution of target classes and categorical features
+  
+  # Obtains distribution of numerical variables
+  
+  # Obtains correlation map
+    
   pass
 
 def get_summary_table():
@@ -35,4 +55,8 @@ def get_violin_plots():
 def get_correlation_map():
   pass
 
+#if __name__ == "__main__":
+  #main(opt[--train_path], opt[--output_file])
   
+df = pd.read_csv('../../data/raw/abalone.data')
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=123)
