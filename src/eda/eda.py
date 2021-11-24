6d20a0ac61f160f1675286c87f8f66c495a9f710
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2021-11-23 21:35:41 -0800

    Add skeleton for eda script

---
+++ src/eda/eda.py
@@ -0,0 +1,38 @@
+# author: DSCI_522_group_28
+# date: 
+
+"""This script constructs various exploratory data visualizations,
+and tables.
+
+Usage: eda.py --train_df=<train_df> --output_file=<output_file>
+
+Options:
+--train_df=<train_df>
+--output=<output>
+""""
+
+
+from doctop import docopt 
+import altair as alt
+import numpy as np
+import pandas as pd
+
+opt = docopt(__doc__)
+
+def main(train, output):
+  
+  pass
+
+def get_summary_table():
+  pass
+
+def get_target_distribution():
+  pass
+
+def get_violin_plots():
+  pass
+
+def get_correlation_map():
+  pass
+
+  
