d51b0b0572d35d165f1bdc258312736a1253c5dc
nickmao <nickmao1994@gmail.com>
2021-11-25 15:15:08 -0800

    Add testing scirpt and two plot scripts

--- src/models/model-testing.py
+++ src/models/model-testing.py
@@ -48,25 +48,29 @@ def main(data_file, out_dir):
     """load the best model and fit on test data
     Parameters
     ----------
-    test : string
-        Path (including filename) to test data (csv file)
+    data_file : string
+        Path to test data
     out_dir : string
         Path to directory where the test result should be saved
     """
     test_df = pd.read_csv(data_file + "/test_df.csv")
     best_model = pickle.load(open(out_dir + "/best_model.sav", "rb"))
 
+    # show the score of best model on test data
     result = test_model(best_model, test_df)
+    
+    # show the coefficients of best model
+    coeff_plot(best_model, out_dir)
 
     
-def test_model(best_model, test_df):
+def test_model(best_model, data_file):
     """test the model on test dataset
     Parameters
     ----------
-    (best_model : sav file
+    best_model : sav file
         the best ML model we have trained
-    test_df : pandas.core.frame.DataFrame
-        a dataframe containing test dataset
+    data_file : string
+        Path to test data
     Returns
     -------
     float
@@ -76,5 +80,23 @@ def test_model(best_model, test_df):
     y_test = test_df['Is old']
     return best_model.score(X_test, y_test)
 
-def coeff_plot(best_model, ):
+def coeff_plot(best_model, out_dir):
+    """output tables and plots
+    Parameters
+    ----------
+    best_model : sav file
+        the best ML model we have trained
+    out_dir : string
+        Path to directory where the test result should be saved
+    """
     
+    feature_names = np.array(best_model[:-1].get_feature_names_out())
+    coeffs = best_model.named_steps["logisticregression"].coef_.flatten()
+    coeff_df = pd.DataFrame(coeffs, index=feature_names, columns=["Coefficient"])
+    coeff_df_sorted = coeff_df.sort_values(by="Coefficient", ascending=False)
+    dfi.export(coeff_df_sorted, out_dir + "/coeff_sorted.png")
+    mglearn.tools.visualize_coefficients(coeffs, feature_names, n_top_features=5)
+    plt.savefig(out_dir + "/coeff_bar.png")
+
+if __name__ == "__main__":
+    main(opt["--train_file"], opt["--out_dir"])
