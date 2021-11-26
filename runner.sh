476ddf37c12715679a750e55fce93506e1ce8bfa
Rakesh Pandey <pandey03@student.ubc.ca>
2021-11-26 12:33:19 -0800

    Create script runner.sh to run the nalysis end to end

--- runner.sh
+++ runner.sh
@@ -9,10 +9,23 @@
 # Usage: bash runner.sh
 
 # Download data
+echo "Downloading data"
 python  src/data/data_download.py  --url="https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data" --outputfile="data/raw1/abalone.data"
 
 # Data preprocessing
+echo "Running data preprocessing script"
 python src/data/data_preprocessing.py --inputfile="data/raw/abalone.data" --out_dir="data/processed"
 
 # EDA
+echo "Running eda script"
 python src/eda/eda.py --data_path="data/processed/train.csv" --out_dir="reports/eda"
+
+# Train
+echo "Running training script"
+python src/models/train.py --data_file="data/processed/train.csv" --out_dir="results/model"
+
+# Test
+echo "Running test script"
+python src/models/test.py --data_file="data/processed/test.csv" --out_dir="results/model"
+
+echo "Exiting! Script successfully completed."
